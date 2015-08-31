227be9a0c56bb8538ec4795bd73888df4060b1e3
Habibie Faried <habibiefaried@gmail.com>
2015-08-31 13:00:54 +0700

    operasi create, update, delete

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 var mongoose = require('mongoose'); mongoose.connect('mongodb://localhost/demo');
 var async = require('async');
+var randomstring = require('randomstring');
 var transaction = require('./transaction.js')(mongoose,async);
 
 var userSchema = new mongoose.Schema({
-  _id: Schema.ObjectId,
+  _id: mongoose.Schema.ObjectId,
   name: String,
   balance: Number,
 });
 var User = mongoose.model('User', userSchema);
 
-
 var query_insert = {
-	collection: "User",
 	act: "insert",
 	data: {
-		name: "Testing",
-		balance: "900",
-	}
-	mongoose_model: User,
-};
-
-var query_update = {
-	collection: "User",
-	act: "update",
-	Id: "55e3cba74ea6565c0d11b48f",
-	data: {
-		name: "Updated",
-		balance: "1290",
+		_id: mongoose.Types.ObjectId(),
+		name: randomstring.generate(10),
+		balance: "10000",
 	},
 	mongoose_model: User,
 };
 
-transaction.apply([query_insert,query_update], function(callback){
-	console.log(callback);
+var query_update = {
+	act: "update",
+	param: {
+		name: "qtYEJhA6w4",
+	}, //parameter update
+	data: {
+		name: "Updatedaa1",
+		balance: "129001",
+	},
+	mongoose_model: User,
+};
+
+var query_delete = {
+	act: "delete",
+	param: {
+		name: "CPnpIBypXg",
+	}, //parameter delete
+	mongoose_model: User,
+};
+
+transaction.apply([query_delete], function(callback){	
+	User.find({}, function(err, users) {
+	  if (err) throw err;
+	  console.log(users);
+	});
 });
--- transaction.js
+++ transaction.js
@@ -37,12 +37,38 @@ module.exports = function(mongoose, async) {
 	trans.apply = function(param, callback){
 		async.forEachSeries(param, function(e, cb) {
 			//console.log(e);
-			
-			cb();
+			if (e.act == "insert") {
+				new e.mongoose_model(e.data).save(function(err){
+					if (err) cb(err);
+					else {
+						console.log("Berhasil");
+						cb();
+					}	
+				});
+				//cb();
+			} else if (e.act == "update") {
+				e.mongoose_model.findOneAndUpdate(e.param,e.data,function(err,result){
+					if (err) {
+						cb(err);
+					}
+					else {
+						console.log("Penanda\n"+result); //previous before update
+						cb();
+					}
+				});
+			} else if (e.act == "delete") {
+				e.mongoose_model.findOneAndRemove(e.param, function(err,result){
+					if (err) cb(err);
+					else console.log("Terhapus\n"+result); //ambil data yang terhapus
+					cb();
+				});
+			} else {
+				console.log("Aksi tidak ditemukan");
+				cb();
+			}
 		}, function(){
 			callback("A");
 		});
-
 	};
 
 	return trans;
